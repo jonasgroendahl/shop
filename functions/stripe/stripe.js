47f84ee6d7efb13088f4675e761bc3578979878a
jonas <jonas.grondahl@wexer.com>
2018-05-01 09:39:48 +0200

    kek

--- functions/stripe/stripe.js
+++ functions/stripe/stripe.js
@@ -1,25 +1,15 @@
 const stripe = require("stripe")(process.env.stripeKey);
-const cors = require("cors");
 
 exports.handler = function(event, context, callback) {
-  if (event.httpMethod != "OPTIONS") {
-    const id = JSON.parse(event.body).id;
-    console.log("CRAZY DAISY", id);
-    stripe.charges.create({
-      amount: 999,
-      currency: "usd",
-      description: "Example charge",
-      source: id
-    });
-  }
+  const id = JSON.parse(event.body).id;
+  const charge = stripe.charges.create({
+    amount: 999,
+    currency: "usd",
+    description: "Example charge",
+    source: id
+  });
   callback(null, {
     statusCode: 200,
-    body: "We did it reddit",
-    headers: {
-      "Access-Control-Allow-Credentials": true,
-      "Access-Control-Allow-Origin": "http://localhost:8888",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-      "Access-Control-Allow-Headers": "X-Requested-With,content-type"
-    }
+    body: "We did it reddit"
   });
 };
